47e00c3b91806f8009871bbce6521d8fa91c284a
atsoc1993 <atsoc1993@gmail.com>
2025-10-06 00:40:13 -0400

    Wallet Modal complete! =)

--- levy/src/App.tsx
+++ levy/src/App.tsx
@@ -1,40 +1,21 @@
 import { useWallet } from '@txnlab/use-wallet-react'
 import { useState } from 'react';
 import './App.css'
+import WalletModal from './components/WalletModal';
 
 export default function App() {
 
   const [selectingWallet, setSelectingWallet] = useState<boolean>(false);
 
-  const { activeAddress, wallets } = useWallet();
+  const { activeAddress } = useWallet();
 
   return (
     <>
-      {!selectingWallet ? activeAddress ? (
-        <h1>Connected Wallet: {activeAddress} </h1>
-      ) : (
-        <button
-          onClick={() => setSelectingWallet(true)}>
-          Connect Wallet
-        </button>
-      ) : null
-      }
-      {selectingWallet &&
-        <>
-          <div id='gray-background'></div>
-          <div id='wallet-selection'>
-            {wallets.map((wallet) =>
-              <div key={wallet.id + 'b'} className='wallet-button'
-                onClick={async () => await wallet.connect()}>
-                <p key={wallet.id + 'p'}>
-                  {wallet.id}
-                </p>
-                <img key={wallet.id + 'img'} className='wallet-icon' src={wallet.metadata.icon}></img>
-              </div>
-            )}
-          </div>
-        </>
-      }
+      {!activeAddress ? 
+      <WalletModal selectingWallet={selectingWallet} setSelectingWallet={setSelectingWallet}/>
+
+    : <h1>Wallet Connected: {activeAddress}</h1>
+    }
     </>
   )
 }
---
+++ levy/src/components/WalletModal.tsx
@@ -0,0 +1,52 @@
+import { type Dispatch, type SetStateAction } from "react"
+import { useWallet } from '@txnlab/use-wallet-react'
+import { FaSkullCrossbones } from 'react-icons/fa';
+
+type Props = {
+    selectingWallet: boolean
+    setSelectingWallet: Dispatch<SetStateAction<boolean>>
+}
+
+export default function WalletModal({ selectingWallet, setSelectingWallet }: Props) {
+  const { wallets } = useWallet();
+
+    return (
+
+        <>
+          <button
+            id='connect-button'
+            className={selectingWallet ? 'shrink' : ''}
+            onClick={() => setSelectingWallet(true)}>
+            {!selectingWallet ? 'Connect Wallet' : ''}
+          </button>
+
+          <>
+            <div id='gray-background'></div>
+            <div id='wallet-selection'
+              className={selectingWallet ? '' : 'shrink'}>
+              {wallets.map((wallet) =>
+                <div key={wallet.id + 'b'} className={selectingWallet ? 'wallet-button' : 'wallet-button shrink'}
+                onClick={async () => await wallet.connect()}>
+                  <p key={wallet.id + 'p'}
+                    className={selectingWallet ? '' : 'shrink'}>
+                    {wallet.id}
+                  </p>
+                  <img key={wallet.id + 'img'}
+                    className={selectingWallet ? 'wallet-icon' : 'wallet-icon shrink'}
+                    src={wallet.metadata.icon}></img>
+                </div>
+              )}
+              <div className={selectingWallet ? 'wallet-button' : 'wallet-button shrink'}
+                onClick={async () => setSelectingWallet(false)}>
+                <p
+                  className={selectingWallet ? '' : 'shrink'}>
+                  Cancel
+                </p>
+                <FaSkullCrossbones style={{alignSelf: 'center', width: '15%', height: '15%'}}/>
+              </div>
+
+            </div>
+          </>
+        </>
+)
+}
